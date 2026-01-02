b7e6624d59a3fd357deddaf52d8fffce5a07a9f6
Mohammed Musab VK <mohammedmusab5069@gmail.com>
2026-01-02 17:54:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import ReactDOM from 'react-dom/client'
+import HomeScreen from './screens/HomeScreen';
+import CounterScreen from './screens/CounterScreen';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <HomeScreen />
+      <CounterScreen />
+      {/* <h1>Hello World!</h1> */}
+    </div>
+   
+  )
+}
+
+export default App
